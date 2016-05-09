612e92ec06713dbb7c065d6aa679a726e8c85396
RaphaelFOURQUET <raphikifourque@gmail.com>
2016-05-09 21:50:21 +0200

    panier

---
+++ JSPStock/src/fr/adaming/panier/AjoutPanierServlet.java
@@ -0,0 +1,66 @@
+package fr.adaming.panier;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.adaming.constante.Constante;
+import fr.adaming.produits.Produit;
+import fr.adaming.produits.ProduitDAO;
+
+/**
+ * Servlet implementation class AjoutPanier
+ */
+@WebServlet("/ajoutPanier")
+public class AjoutPanierServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	private ProduitDAO produitDAO;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AjoutPanierServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//recuperer l id en parametre
+		int id = Integer.parseInt(request.getParameter(Constante.ID));
+		//System.out.println(id);
+		
+		//Recuperer le produit associé
+		//Produit p = Donnees.findProduit(id);
+		//System.out.println(p);
+		
+		Produit p = produitDAO.findProduit(id);
+		
+		//Recuperation panier + ajout
+		Panier panier = (Panier) request.getSession().getAttribute("panier");
+		panier.addProduit(p);
+		
+		//on repasse le panier
+		request.getSession().setAttribute("panier", panier);
+		
+		//Rediriger
+		response.sendRedirect(Constante.PRODUIT);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ JSPStock/src/fr/adaming/panier/DeleteFromPanierServlet.java
@@ -0,0 +1,38 @@
+package fr.adaming.panier;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.adaming.constante.Constante;
+
+/**
+ * Servlet implementation class DeleteFromPanierServlet
+ */
+@WebServlet("/deleteFromPanier")
+public class DeleteFromPanierServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//recuperer id et panier
+		int index = Integer.parseInt(request.getParameter("ligne"))-1;//RFRF : -1 car le status renverra ligne=1 au depart au lieu de 0
+		Panier panier = (Panier) request.getSession().getAttribute("panier");
+		
+		//supprimer du panier
+		panier.deleteAtIndex(index);
+		
+		//on repasse le panier
+		request.getSession().setAttribute("panier", panier);
+		
+		//redirect
+		response.sendRedirect(Constante.PRODUIT);
+		
+	}
+
+}
